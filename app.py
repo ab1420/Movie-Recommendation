28f89ef815d795dd640b689af678538484282c57
AnandAhir <56204503+ab1420@users.noreply.github.com>
2021-07-02 14:46:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify, render_template, request
 from model import recommend_movie
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('mymodel.pkl', 'rb'))
 
 @app.route('/')
 def home():
